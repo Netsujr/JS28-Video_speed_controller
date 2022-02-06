fc9afcc37538a24a76784438b5bcf9046d3aa5af
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-06 15:20:30 +0900

    Chalenge finished
    added a function that only changes the speed when the bar is clicked

---
+++ app.js
@@ -0,0 +1,21 @@
+// @ts-nocheck
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+
+function handleMove(e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + '%';
+  bar.style.height = height;
+  const playbackRate = percent * (max - min) + min;
+  bar.textContent = playbackRate.toFixed(2) + 'x';
+  speed.addEventListener('click', () => {video.playbackRate = playbackRate});
+  // console.log(height);
+  // speed only changes when the bar is clicked
+};
+
+
+speed.addEventListener('mousemove', handleMove);
